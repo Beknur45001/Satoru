58e93c6014a1eb25283d6b94e7e9b26ee6ce0023
Beknur <b4476125@gmail.com>
2025-01-30 09:50:08 +0600

    added bbbb

--- homework/index.js
+++ homework/index.js
@@ -1,10 +1,14 @@
 const icon = document.querySelector(".icon");
 const search = document.querySelector(".ooo");
 const clear = document.querySelector(".clear");
+let abaut = document.querySelector(".abaut")
+
+abaut = "Arsen Hello"
+
 
 icon.onclick = function(){
     search.classList.toggle("active")
 }
 clear.onclick = function(){
-   document.getElementById("search").value = "  ";
+   document.getElementById("search").value = "";
 }
